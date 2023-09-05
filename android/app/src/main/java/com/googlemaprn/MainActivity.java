3d3c88a80607d5044dc0ca89f696a017cd2847c5
prabhat-cn <prabhat@capitalnumbers.com>
2023-09-05 18:24:25 +0530

    App icon, Package, Name changed

--- android/app/src/main/java/com/googlemaprn/MainActivity.java
+++ android/app/src/main/java/com/googlemaprn/MainActivity.java
@@ -1,4 +1,5 @@
-package com.googlemaprn;
+package com.techhub.findplaces;
+
 import expo.modules.ReactActivityDelegateWrapper;
 
 import com.facebook.react.ReactActivity;
@@ -38,10 +39,11 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled()));
+    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+        new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
+            // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            DefaultNewArchitectureEntryPoint.getFabricEnabled()));
   }
 }
